9be82ed665a0f65fae813286cb2ebe66b06953be
rosskarchner <ross@karchner.com>
2014-12-16 18:23:04 +0000

    the rest of the project

---
+++ DC_Tech_Events/DC_Tech_Events/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for DC_Tech_Events project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DC_Tech_Events.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
